999d0f19689c38d6610f6683ef562ed13be6b652
liusong <liusong1111@gmail.com>
2011-02-04 20:19:46 +0800

    .

--- config/Routes.scala
+++ config/Routes.scala
@@ -7,4 +7,6 @@ object MyRoutes extends Routes{
   get("/users", by[UsersController]("index"))
   get("/users/1", by[UsersController]("show"))
   get("/search", by[SearchPortalController]("index"))
+
+  get("/admin/manage_search", by[SearchManageController]("index"))
 }
